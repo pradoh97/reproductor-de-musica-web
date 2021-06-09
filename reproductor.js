54ca2eba96fb6d5275301b6c8171bad0f5614624
pradoh97 <pradoh97@gmail.com>
2021-06-09 12:03:44 -0300

    Add files via upload

--- reproductor.js
+++ reproductor.js
@@ -1,72 +1,83 @@
 window.addEventListener('load', iniciarReproductor);
 
-let idFrame, boton = [], deslizador = [], reproductor, audio, cancion, caratula, duracion, duracionDOM,
-    caratulaDOM, listadoCanciones = ['A Veces se Gana, a Veces se Pierde', 'Indie Explicit',
-                                    'Quien Sepa lo Que es Correcto, Hará lo Correcto'];
-    icono = [], uris = {musica: 'Musica/', caratula: 'Caratulas/'}, reproduciendo = 0;
+let idFrame, listadoCanciones = ['A Veces se Gana, a Veces se Pierde', 'Indie Explicit',
+                                  'Quien Sepa lo Que es Correcto, Hará lo Correcto'];
+let icono = [], uris = {musica: 'Musica/', caratula: 'Caratulas/'}, reproduciendo = 0;
+let cancion = {
+  audio: new Audio(),
+  URI: '',
+  caratula:'',
+  duracion:''
+};
+let reproductor = {
+  nodo: '',
+  duracion: '',
+  caratula: '',
+  deslizador: [],
+  boton: []
+};
 
-icono['pausa'] = 'fa-pause';
-icono['reproducir'] = 'fa-play';
-icono['volumenSilenciado'] = 'fas fa-volume-mute';
-icono['volumenBajo'] = 'fas fa-volume-down';
+icono['pausa'] = 'fa-pause', icono['reproducir'] = 'fa-play',
+icono['volumenSilenciado'] = 'fas fa-volume-mute', icono['volumenBajo'] = 'fas fa-volume-down',
 icono['volumenAlto'] = 'fas fa-volume-up';
 
 function iniciarReproductor(){
-  boton['reproducirPausa'] = document.querySelector('.controles__reproduccion .' + icono['reproducir']).parentElement;
-  boton['cancionSiguiente'] = document.querySelector('.controles__reproduccion .fa-step-forward').parentElement;
-  boton['cancionAnterior'] = document.querySelector('.controles__reproduccion .fa-step-backward').parentElement;
-  boton['volumen'] = document.querySelector('.controles__volumen button');
-  deslizador['volumen'] = document.querySelector('.controles__volumen input');
-  deslizador['progresoCancion'] = document.querySelector('.reproduccion__progreso input');
+  reproductor.boton['reproducirPausa'] = document.querySelector('.controles__reproduccion .' + icono['reproducir']).parentElement;
+  reproductor.boton['cancionSiguiente'] = document.querySelector('.controles__reproduccion .fa-step-forward').parentElement;
+  reproductor.boton['cancionAnterior'] = document.querySelector('.controles__reproduccion .fa-step-backward').parentElement;
+  reproductor.boton['volumen'] = document.querySelector('.controles__volumen button');
+  reproductor.deslizador['volumen'] = document.querySelector('.controles__volumen input');
+  reproductor.deslizador['progresoCancion'] = document.querySelector('.reproduccion__progreso input');
 
-  caratulaDOM = document.querySelector('.cancion__caratula img');
-  duracionDOM = document.querySelector('.reproduccion__progreso time');
-  reproductor = document.querySelector('.reproductor');
+  reproductor.caratula = document.querySelector('.cancion__caratula img');
+  reproductor.duracion = document.querySelector('.reproduccion__progreso time');
+  reproductor.nodo = document.querySelector('.reproductor');
 
-  boton['reproducirPausa'].addEventListener('click', alternarReproduccion);
-  boton['cancionSiguiente'].addEventListener('click', () => cargarCancion(1));
-  boton['cancionAnterior'].addEventListener('click', () => cargarCancion(-1));
-  boton['volumen'].addEventListener('click', alternarDeslizadorVolumen);
+  reproductor.boton['reproducirPausa'].addEventListener('click', alternarReproduccion);
+  reproductor.boton['cancionSiguiente'].addEventListener('click', () => cargarCancion(1));
+  reproductor.boton['cancionAnterior'].addEventListener('click', () => cargarCancion(-1));
+  reproductor.boton['volumen'].addEventListener('click', alternarDeslizadorVolumen);
   document.addEventListener('click', alternarDeslizadorVolumen);
-  deslizador['volumen'].addEventListener('input', moverVolumen);
-  deslizador['progresoCancion'].addEventListener('input', moverProgreso);
+  reproductor.deslizador['volumen'].addEventListener('input', moverVolumen);
+  reproductor.deslizador['progresoCancion'].addEventListener('input', moverProgreso);
 
-  audio = new Audio();
   cargarCancion(reproduciendo);
 }
 
 function moverProgreso(e){
   let momento = e.target.value;
-  audio.fastSeek(momento);
+  cancion.audio.fastSeek(momento);
 }
 
 function cargarCancion(sentido){
   let cambiarA = reproduciendo + sentido;
+  reproductor.caratula.classList.add('oculto');
 
   if(cambiarA >= listadoCanciones.length) reproduciendo = 0;
   else if(cambiarA < 0) reproduciendo = listadoCanciones.length - 1;
   else reproduciendo = cambiarA;
 
-  cancion = uris.musica + listadoCanciones[reproduciendo] + '.mp3';
-  caratula = uris.caratula + listadoCanciones[reproduciendo] + '.jpg';
+  cancion.URI = uris.musica + listadoCanciones[reproduciendo] + '.mp3';
+  cancion.caratula = uris.caratula + listadoCanciones[reproduciendo] + '.jpg';
+  cancion.audio.src = cancion.URI;
 
-  audio.src = cancion;
-  caratulaDOM.src = caratula;
-  caratulaDOM.classList.remove('oculto');
-  deslizador['progresoCancion'].value = 0;
+  reproductor.caratula.src = cancion.caratula;
+  reproductor.caratula.classList.remove('oculto');
+
+  reproductor.deslizador['progresoCancion'].value = 0;
 
   setTimeout( () => cambiarCancion(), 5000);
 }
 
 function cambiarCancion(){
-  duracion = duracionCancion(audio.duration);
+  cancion.duracion = duracionCancion(cancion.audio.duration);
 
-  duracionDOM.innerText = `00:00/${duracion.minutos}:${duracion.segundos}`;
-  deslizador['progresoCancion'].max = audio.duration;
+  reproductor.duracion.innerText = `00:00/${cancion.duracion.minutos}:${cancion.duracion.segundos}`;
+  reproductor.deslizador['progresoCancion'].max = cancion.audio.duration;
 
   document.querySelector('.cancion__titulo').innerText = listadoCanciones[reproduciendo];
 
-  if(boton['reproducirPausa'].firstChild.classList.contains(icono['pausa'])) audio.play();
+  if(reproductor.boton['reproducirPausa'].firstChild.classList.contains(icono['pausa'])) cancion.audio.play();
 }
 
 function duracionCancion(duracionS){
@@ -80,39 +91,43 @@ function duracionCancion(duracionS){
 function actualizarReproductor(){
   idFrame = requestAnimationFrame(actualizarReproductor);
 
-  let momentoActual = duracionCancion(audio.currentTime);
-  duracionDOM.innerText = `${momentoActual.minutos}:${momentoActual.segundos}/${duracion.minutos}:${duracion.segundos}`;
-  deslizador['progresoCancion'].value = audio.currentTime;
+  let momentoActual = duracionCancion(cancion.audio.currentTime);
+  reproductor.duracion.innerText = `${momentoActual.minutos}:${momentoActual.segundos}/${cancion.duracion.minutos}:${cancion.duracion.segundos}`;
+
+  reproductor.deslizador['progresoCancion'].value = cancion.audio.currentTime;
+
+  //Si terminó la canción, cambiar a la siguiente.
+  if(cancion.audio.currentTime == cancion.audio.duration) cargarCancion(1);
 }
 
 function alternarReproduccion(){
-  let pausar = boton['reproducirPausa'].firstChild.classList.toggle(icono['reproducir']);
-  boton['reproducirPausa'].firstChild.classList.toggle(icono['pausa']);
+  let pausar = reproductor.boton['reproducirPausa'].firstChild.classList.toggle(icono['reproducir']);
+  reproductor.boton['reproducirPausa'].firstChild.classList.toggle(icono['pausa']);
 
   if(!pausar){
     idFrame = requestAnimationFrame(actualizarReproductor);
-    audio.play();
+    cancion.audio.play();
   } else {
     window.cancelAnimationFrame(idFrame);
-    audio.pause();
+    cancion.audio.pause();
   }
 }
 
 function alternarDeslizadorVolumen(e){
   e.stopPropagation();
-  if(e.target == boton['volumen'] || e.target == boton['volumen'].firstChild){
-    deslizador['volumen'].classList.toggle('oculto');
+  if(e.target == reproductor.boton['volumen'] || e.target == reproductor.boton['volumen'].firstChild){
+    reproductor.deslizador['volumen'].classList.toggle('oculto');
   } else {
-    deslizador['volumen'].classList.add('oculto');
+    reproductor.deslizador['volumen'].classList.add('oculto');
   }
 }
 
 function moverVolumen(e){
   let volumen = e.target.value;
 
-  audio.volume = volumen/100;
+  cancion.audio.volume = volumen/100;
 
-  let iconoVolumen = boton['volumen'].querySelector('i');
+  let iconoVolumen = reproductor.boton['volumen'].querySelector('i');
 
   if(volumen == 0) iconoVolumen.className = icono['volumenSilenciado'];
   else if(volumen <= 50) iconoVolumen.className = icono['volumenBajo'];
